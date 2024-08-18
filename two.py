eec5fffb095eebca2393b36a19142762fde5a8c2
timofeeva-olya <olya.sinyakova.20.02@mail.ru>
2024-08-18 18:09:47 +0300

    Add files via upload

---
+++ two.py
@@ -0,0 +1,5 @@
+number_of_tasks=12
+hours_spent = 1.5
+course_Python='Python'
+Time_for_one_task = 1.5/12
+print ('Курс:',course_Python ,',всего задач:', number_of_tasks ,',затрачено часов:', hours_spent, ',среднее время выполнения ' ,Time_for_one_task, 'часа.' )
